924a9825a28c2fa9f3fbd97332cb718bc71dc8e3
Boris <superbb@users.noreply.github.com>
2016-06-21 04:17:38 -0700

    typo via auto extension

--- exercise1/loading_and_modeling/load_data_lake.sh
+++ exercise1/loading_and_modeling/load_data_lake.sh
@@ -15,13 +15,28 @@ tail -n +2 /home/w205/hosp_temp/'Hospital General Information.csv' > /home/w205/
 tail -n +2 /home/w205/hosp_temp/'Timely and Effective Care - Hospital.csv' > /home/w205/hosp_temp/no_head/effective_care.csv 
 tail -n +2 /home/w205/hosp_temp/'Readmissions and Deaths - Hospital.csv' > /home/w205/hosp_temp/no_head/readmissions.csv 
 tail -n +2 /home/w205/hosp_temp/'Measure Dates.csv' > /home/w205/hosp_temp/no_head/Measures.csv 
-tail -n +2 /home/w205/hosp_temp/'hvbp_hcahps_05_28_2015.csv' > /home/w205/hosp_temp/no_head/surveys_responses.csv
+tail -n +2 /home/w205/hosp_temp/'hvbp_hcahps_05_28_2015.csv' > /home/w205/hosp_temp/no_head/survey_responses.csv
 
-#make the hospital_compare directory
+#clear old directory (if it exists)
+hdfs dfs -rm -r /user/w205/hospital_compare
+
+#make the hospital_compare directories
 hdfs dfs -mkdir -p /user/w205/hospital_compare
+hdfs dfs -mkdir -p /user/w205/hospital_compare/hospitals
+hdfs dfs -mkdir -p /user/w205/hospital_compare/effective_care
+hdfs dfs -mkdir -p /user/w205/hospital_compare/readmissions
+hdfs dfs -mkdir -p /user/w205/hospital_compare/measures
+hdfs dfs -mkdir -p /user/w205/hospital_compare/survey_responses
 
 #load raw (headerless) files into hdfs
-hdfs dfs -put /home/w205/hosp_temp/no_head/* /user/w205/hospital_compare
+hdfs dfs -put /home/w205/hosp_temp/no_head/hospitals.csv /user/w205/hospital_compare/hospitals/
+hdfs dfs -put /home/w205/hosp_temp/no_head/effective_care.csv /user/w205/hospital_compare/effective_care/
+hdfs dfs -put /home/w205/hosp_temp/no_head/readmissions.csv /user/w205/hospital_compare/readmissions/
+hdfs dfs -put /home/w205/hosp_temp/no_head/Measures.csv /user/w205/hospital_compare/measures/
+hdfs dfs -put /home/w205/hosp_temp/no_head/survey_responses.csv /user/w205/hospital_compare/survey_responses/
+
+
+
 
 #to check that they're there run 
 #hdfs dfs -ls /user/w205/hospital_compare
